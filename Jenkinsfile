213f5f6b4a0388f3ce9a1596f75fa7c8b83c58c8
prabhat-roy <prabhatkumar.roy@outlook.com>
2023-12-23 18:02:07 +0530

    m

---
+++ Jenkinsfile
@@ -0,0 +1,145 @@
+def gv_script
+pipeline {
+    agent { label 'Jenkins-Agent' }
+    environment {
+        NEXUS_IP = "10.0.1.6"	
+        K8S_MASTER_IP ="10.0.1.3"        
+        nexus_cred = "nexus"
+		NEXUS_IMAGE_URL = "${NEXUS_IP}:8082"
+        IMAGE_NAME = "docker-images" 
+        DOCKERHUB_NAME = "prabhatrkumaroy"
+		GITHUB_URL = "https://github.com/prabhat-roy/java-kubernetes.git"
+    }
+    tools {
+        jdk 'Java'
+        maven 'Maven'
+    }
+    stages {
+        stage("Init") {
+            steps {
+                script {
+                    gv_script = load"script.groovy"
+                }
+            }
+        }
+        stage("Cleanup Workspace") {
+            steps {
+                script {
+                    gv_script.cleanup()
+                }
+            }
+        }
+        stage("Checkout from Git Repo") {
+            steps {
+                script {
+                    gv_script.checkout()
+                }
+            }
+        }
+        stage("OWASP FS Scan") {
+            steps {
+                script {
+                    gv_script.owasp()
+                }
+            }
+        }
+        stage("SonarQube Analysis") {
+            steps {
+                script {
+                    gv_script.sonaranalysis()
+                }
+            }
+        }
+        stage("Trivy FS Scan") {
+            steps {
+                script {
+                    gv_script.trivyfs()
+                }
+            }
+        }
+        stage("Code Compile") {
+            steps {
+                script {
+                    gv_script.codecompile()
+                }
+            }
+        }
+        stage("Building Application") {
+            steps {
+                script {
+                    gv_script.buildapplication()
+                }
+            }
+        }
+        stage("Docker Build") {
+            steps {
+                script {
+                    gv_script.dockerbuild()
+                }
+            }
+        }
+        stage("Trivy Image Scan") {
+            steps {
+                script {
+                    gv_script.trivyimage()
+                }
+            }
+        }
+        stage("Docker Scout Image Scan") {
+            steps {
+                script {
+                    gv_script.dockerscout()
+                }
+            }
+        }
+        stage("Grype Image Scan") {
+            steps {
+                script {
+                    gv_script.grype()
+                }
+            }
+        }
+        stage("Syft Image Scan") {
+            steps {
+                script {
+                    gv_script.syft()
+                }
+            }
+        }
+        stage("Docker Push Docker Hub") {
+            steps {
+                script {
+                    gv_script.dockerhub()
+                }
+            }
+        }
+        stage("Docker Push Nexus") {
+            steps {
+                script {
+                    gv_script.dockernexus()
+                }
+            }
+        }
+       stage("Deployment to K8s") {
+            steps {
+                script {
+                    gv_script.kubernetes()
+                }
+            }
+        }
+        stage("Container Removal") {
+            steps {
+                script {
+                    gv_script.removedocker()
+                }
+            }
+        }
+    }
+    post {
+        always {
+            sh "docker logout"
+            deleteDir()
+        }
+    }
+}
+
---
+++ script.groovy
@@ -0,0 +1,104 @@
+def cleanup() {
+        cleanWs()
+}
+
+def checkout() {
+        git branch: 'main', credentialsId: 'github', url: "$GITHUB_URL"
+}
+
+def owasp() {
+    dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-check'
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+}
+
+def sonaranalysis() {
+        withSonarQubeEnv(installationName: 'SonarQube', credentialsId: 'sonar') {
+            sh "mvn sonar:sonar"
+    }
+}
+
+def qualitygate() {
+        waitForQualityGate abortPipeline: false, credentialsId: 'sonar'
+}
+
+def trivyfs() {
+        sh "trivy fs ."
+}
+
+def codecompile() {
+        sh "mvn clean compile"
+}
+
+def buildapplication() {
+    sh "mvn clean install"
+}
+
+def dockerbuild() {
+        sh "docker build . -t ${IMAGE_NAME}:${BUILD_NUMBER}"
+}
+
+def trivyimage() {
+        sh "trivy image ${IMAGE_NAME}:${BUILD_NUMBER}"
+}
+
+def grype() {
+        sh "grype ${IMAGE_NAME}:${BUILD_NUMBER}"       
+}
+
+def syft() {
+        sh "syft ${IMAGE_NAME}:${BUILD_NUMBER}"       
+}
+
+def dockerrun() {
+    sh '''
+        docker run -dt ${IMAGE_NAME}:${BUILD_NUMBER}"
+        docker stop ${IMAGE_NAME}:${BUILD_NUMBER}"
+        '''
+}
+
+def dockerhub() {
+        withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+               sh "docker image tag ${IMAGE_NAME}:${BUILD_NUMBER} ${DOCKERHUB_NAME}/${IMAGE_NAME}:${BUILD_NUMBER}"
+               sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+               sh "docker push ${DOCKERHUB_NAME}/${IMAGE_NAME}:${BUILD_NUMBER}"
+               sh "docker pull ${DOCKERHUB_NAME}/${IMAGE_NAME}:${BUILD_NUMBER}"
+               sh "docker rmi -f ${DOCKERHUB_NAME}/${IMAGE_NAME}:${BUILD_NUMBER}"
+    }
+}
+
+def dockernexus() {
+        withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'nexusPassword', usernameVariable: 'nexusUser')]) {
+                sh "docker image tag ${IMAGE_NAME}:${BUILD_NUMBER} ${NEXUS_IMAGE_URL}/${IMAGE_NAME}:${BUILD_NUMBER}"
+	        sh "docker login -u ${env.nexusUser} -p ${env.nexusPassword} ${NEXUS_IMAGE_URL}"
+                sh "docker push ${NEXUS_IMAGE_URL}/${IMAGE_NAME}:${BUILD_NUMBER}"
+                sh "docker pull ${NEXUS_IMAGE_URL}/${IMAGE_NAME}:${BUILD_NUMBER}"
+                sh "docker rmi -f ${NEXUS_IMAGE_URL}/${IMAGE_NAME}:${BUILD_NUMBER}"
+        }
+}
+
+def dockerscout() {
+        withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+                sh "docker scout quickview ${IMAGE_NAME}:${BUILD_NUMBER}"
+                sh "docker scout cves ${IMAGE_NAME}:${BUILD_NUMBER}"
+                sh "docker scout recommendations ${IMAGE_NAME}:${BUILD_NUMBER}"
+        }
+}
+
+def kubernetes() {
+                 sshagent(['k8s']) {
+                        sh "scp -o StrictHostKeyChecking=no service.yml nexus-deployment.yml dockerhub-deployment.yml root@'${K8S_MASTER_IP}':/root"
+                        sh "ssh root@'${K8S_MASTER_IP}' kubectl apply -f ."
+                        sh "ssh root@'${K8S_MASTER_IP}' rm -rf *.yml"
+        }
+}
+
+def removedocker() {
+                sh "docker rmi -f ${IMAGE_NAME}:${BUILD_NUMBER}"
+                sh "docker rmi -f owasp/zap2docker-stable"
+                sh "docker system prune --force --all"
+                sh "docker system prune --force --all --volumes"
+}
+
+
+return this
